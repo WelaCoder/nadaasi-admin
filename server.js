d7c0627386c7bf388b154166c787bc226d7af7af
Zain Ul Abideen <zain.abideen14572@gmail.com>
2020-09-15 19:33:42 +0500

    ...

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+// connectDB();
 // app.get("/uploads/:file", (req, res) => {
 //   res.sendFile(__dirname + "/client/public/uploads/" + req.params.file);
 // });
@@ -18,13 +18,10 @@ connectDB();
 
 //   return res.sendFile(__dirname + "/mynewfile3.html");
 // });
-app.get("/payment", (req, res) => {
-  fs.writeFile("mynewfile3.html", req.body.html, function (err) {
-    if (err) throw err;
-    console.log("Saved!");
-  });
-
-  return res.sendFile(__dirname + "/mynewfile3.html");
+const root = require("path").join(__dirname, "client", "build");
+app.use(express.static(root));
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root });
 });
 app.use(express.json({ extended: true }));
 // app.use("/api/user", require("./routes/user"));
