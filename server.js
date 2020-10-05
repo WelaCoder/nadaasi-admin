e2e818bed6dce02caee438b6524956dd5d2fceba
Zain Ul Abideen <zain.abideen14572@gmail.com>
2020-10-05 19:15:04 +0500

    ..

--- server.js
+++ server.js
@@ -5,15 +5,7 @@ var fs = require("fs");
 const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
-app.use(function (req, res, next) {
-  if (req.secure) {
-    // request was via https, so do no special handling
-    next();
-  } else {
-    // request was via http, so redirect to https
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
+
 // connectDB();
 // app.get("/uploads/:file", (req, res) => {
 //   res.sendFile(__dirname + "/client/public/uploads/" + req.params.file);
