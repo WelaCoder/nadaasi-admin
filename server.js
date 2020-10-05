f749d70237f612854a1da0887ce3ddf378dcbaa0
Zain Ul Abideen <zain.abideen14572@gmail.com>
2020-10-05 19:34:17 +0500

    ..

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 var fs = require("fs");
+var secure = require('express-force-https');
 
 const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
+app.use(secure);
 
 // connectDB();
 // app.get("/uploads/:file", (req, res) => {
